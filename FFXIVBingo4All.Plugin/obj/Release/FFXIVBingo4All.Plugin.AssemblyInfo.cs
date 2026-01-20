50dbd018c1329de5ca2b87cf6d547d921bd1a779
Kei Joi <kei.joi.ffxiv@gmail.com>
2026-01-19 18:32:48 -0600

    Rebuilt Plugin

--- FFXIVBingo4All.Plugin/obj/Release/FFXIVBingo4All.Plugin.AssemblyInfo.cs
+++ FFXIVBingo4All.Plugin/obj/Release/FFXIVBingo4All.Plugin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Host relay plugin for multi-card bingo sessions.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a387d9e075932a31fcf475132e7d94f6158df9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+582ab1892446b04e7dba7af199cef6540cad31fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FFXIVBingo4All")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FFXIVBingo4All")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
